65a0ef9ffb88d264420835ebbaec59e1090a3dec
mohdadil1 <39762134+mohdadil1@users.noreply.github.com>
2024-02-01 17:06:46 +0000

    conversion on datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,21 @@
+let score="Adil"
+//console.log(typeof score);
+//console.log(typeof(score));
+let valueInNumber=Number(score)
+//console.log(valueInNumber);
+// "33"=> 33
+//"33abc" => NaN
+//true =>1 ; false => 0
+
+let isLoggedIn="adil"
+let boolean=Boolean(isLoggedIn)
+console.log(typeof(boolean));
+console.log( boolean);
+//1 => true
+//0 => false
+//"" => false
+//"Adil" => true
+let number=5.8
+let stringnumber=String(number)
+console.log(typeof(stringnumber));
+console.log(stringnumber);
