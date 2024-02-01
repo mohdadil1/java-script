8abbba690d0ff6ae468339d6d43a73d2fd36d94c
mohdadil1 <39762134+mohdadil1@users.noreply.github.com>
2024-02-01 15:08:05 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=1233
+let accountEmail="adilm0@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+//accountId=2 not allowed
+console.log(accountId);
+console.log(accountEmail);
+/*
+prefer not to use var
+because issue in use of block scope and fuction scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,16 @@
+"use strict";// treat all js code as newer version
+//alert(3 + 3)//we are using node js not browser
+console.log(3+ 3);
+console.log("Adil");
+let name="adil"
+let age=18
+let isLoggedIn=false
+
+// number==> 2 to power 53
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol =>
+// object
+console.log(typeof undefined);
