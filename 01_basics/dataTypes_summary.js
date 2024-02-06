f1a797ada96b6c569d3d896776edf3c7e7d565f2
mohdadil1 <39762134+mohdadil1@users.noreply.github.com>
2024-02-06 00:35:26 +0000

    dataTypes_summry

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,24 @@
+//permitive
+//7 types : String,Number,Boolean,null,undefined,Symbol,BigInt
+
+const score=100
+const scoreValue=100.3
+const IsLogged=false
+const outSide=undefined
+let userEmail;
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id==anotherId);
+const bigNumber=34554357664356754n
+
+//Reference(Non primitive)
+//Array,Object,Function
+const heros=["shaktiman","naagraj","doga"]
+let myObj={
+    name:"Adil",
+    age:26
+}
+const myFunction=function(){
+    console.log("Hello World");
+}
+console.log(typeof bigNumber);
