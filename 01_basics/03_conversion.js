6cad642d6f84d28662f4a9efb3dde68a73da133c
mohdadil1 <39762134+mohdadil1@users.noreply.github.com>
2024-02-06 00:01:38 +0000

    conversion_and_operation

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -9,13 +9,32 @@ let valueInNumber=Number(score)
 
 let isLoggedIn="adil"
 let boolean=Boolean(isLoggedIn)
-console.log(typeof(boolean));
-console.log( boolean);
+// console.log(typeof(boolean));
+// console.log( boolean);
 //1 => true
 //0 => false
 //"" => false
 //"Adil" => true
 let number=5.8
 let stringnumber=String(number)
-console.log(typeof(stringnumber));
-console.log(stringnumber);
+// console.log(typeof(stringnumber));
+// console.log(stringnumber);
+//*****************operations***************** */
+let value=3
+let negvalue=-value
+// console.log(negvalue);
+let str1="mohd"
+let str2="Adil"
+let str3=str1+str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(+true);
+// console.log(+"");
+let num1,num2,num3
+num1=num2=num3=2+2
+let gamecounter=100
+gamecounter++
+console.log(gamecounter);
