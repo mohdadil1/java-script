69daeeaa99dd8a8d72af3d2ad26c049cc65da898
mohdadil1 <39762134+mohdadil1@users.noreply.github.com>
2024-02-06 01:35:27 +0000

    Stings

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,20 @@
+const name="Adil"
+const repoCount=14
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+const gameName=new String('Adil-dil')
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt('2'));
+// console.log(gameName.indexOf('i'));
+const newString=gameName.substring(1,5)
+// console.log(newString);
+const anotherString=gameName.slice(-8,4)
+// console.log(anotherString);
+const str1="   adil     "
+const str2=str1.trim()
+console.log(str2.length);
+const url="https://.apple2024.com"
+console.log(url.replace('2024','-'));
+console.log(url.includes('Adil'));
+console.log(gameName.split('-'));
