4ea69aaba7bd620a5959f6d36364a51a0058437c
mohdadil1 <39762134+mohdadil1@users.noreply.github.com>
2024-02-09 23:50:00 +0000

    Dates

---
+++ 01_basics/07_Dates.js
@@ -0,0 +1,12 @@
+//Dates
+let myDate=new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString('en-IN'));
+// console.log(typeof myDate);
+let mycreatedDate= new Date(1998,0,14)
+// console.log(mycreatedDate.toDateString());
+let myTimeStamp=Date.now()
+// console.log(myTimeStamp);
+let newDate = new Date()
+console.log(newDate.getHours());
