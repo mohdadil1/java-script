63b6e839daf33df8fb7c19c77f1165c36f41abc2
mohdadil1 <adilm925@gmail.com>
2024-03-23 00:42:21 +0530

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,39 @@
+// // for 
+// for (let i=0;i<=10;i++){
+//     if(i==5){
+//         console.log("5 is BEST number 5");
+
+//     }
+// }
+// for (let i =0;i<=10;i++){
+//     console.log(`out loop valur:${i}`);
+//     for(let j=0;j<=10;j++){
+//         // console.log(`inner loop value${j} and inner loop ${i}`);
+//         console.log(i+'*'+j+`=`+i*j);
+//     }
+// }
+// let myArray=["flash","batman","superman"]
+// console.log(myArray.length);
+// for(let index=0;index<=myArray.length;index++){
+//     const element=myArray[index];
+//     console.log(element);
+// }
+// break and continue
+// for(let i=1;i<=20;i++){
+//     if(i==5){
+//         console.log(`detected 5`);
+//         break;
+//     }
+
+//     console.log(`values of i is ${i}`);
+    
+// }
+for(let i=1;i<=20;i++){
+    if(i==5){
+        console.log(`detected 5`);
+        continue;
+    }
+
+    console.log(`values of i is ${i}`);
+    
+}
