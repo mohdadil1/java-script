fcea4424201fd4abfb1cf81f8d1561f0729b8ac0
mohdadil1 <adilm925@gmail.com>
2024-03-26 15:11:18 +0530

    Projects

--- Projects/3-DigitalClock/Mohdadil.js
+++ Projects/3-DigitalClock/Mohdadil.js
@@ -1,1 +1,6 @@
-console.log("hello world ");
+//  const clock=document.getElementById('clock');
+ const clock=document.querySelector('#clock');
+setInterval(function(){
+let date= new Date();
+clock.innerHTML=date.toLocaleTimeString();   
+},1000);
